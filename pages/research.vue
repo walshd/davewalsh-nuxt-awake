d2644a14f31b1489a96440322d1f0cb4c9b7e068
Dave <walshd@edgehill.ac.uk>
2019-12-13 20:20:13 +0000

    added the few pages

---
+++ pages/research.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="about-page" class="page-wrapper about-page content-page">
+    <site-hero
+      title="Dave's Research"
+      subtitle=""
+      image="/uploads/davewalsh-hero.jpg"
+    ></site-hero>
+    <main-section theme="sidebar-right">
+      <template v-slot:default>
+        <div class="content">
+          <h3>Digital Cultural Heritage</h3>
+          <p>My research is in the field of .....
+          </p>
+
+      </template>
+
+
+    </main-section>
+  </div>
+</template>
+<script>
+import QuoteCard from '~/components/cards/QuoteCard'
+export default {
+  head() {
+    return {
+      title: `Research | ${this.$siteConfig.siteName}`
+    }
+  },
+  components: { QuoteCard }
+}
+</script>
---
+++ pages/work.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="about-page" class="page-wrapper about-page content-page">
+    <site-hero
+      title="Dave's Work"
+      subtitle=""
+      image="/uploads/davewalsh-hero.jpg"
+    ></site-hero>
+    <main-section theme="sidebar-right">
+      <template v-slot:default>
+        <div class="content">
+          <h3>Web work</h3>
+          <p>My web work .....
+          </p>
+
+      </template>
+
+
+    </main-section>
+  </div>
+</template>
+<script>
+import QuoteCard from '~/components/cards/QuoteCard'
+export default {
+  head() {
+    return {
+      title: `Research | ${this.$siteConfig.siteName}`
+    }
+  },
+  components: { QuoteCard }
+}
+</script>
